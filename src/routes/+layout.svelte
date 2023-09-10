8f75f3de1db59c6a294393fa53a25cc47292be64
Lucyna <santoryo.evergarden@gmail.com>
2023-09-10 23:01:50 +0200

    postcss to css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,63 +1,64 @@
 <script>
-	import '../app.postcss';
-	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
-	import Navigation from '$lib/Navigation/Navigation.svelte';
-	import { initializeStores, Drawer, getDrawerStore } from '@skeletonlabs/skeleton';
-	import { LightSwitch } from '@skeletonlabs/skeleton';
-	import { onMount } from 'svelte'
-	import { invalidate } from '$app/navigation'
-	initializeStores();
+  import "../app.postcss";
+  import "../app.postcss";
+  import { AppShell, AppBar } from "@skeletonlabs/skeleton";
+  import Navigation from "$lib/Navigation/Navigation.svelte";
+  import {
+    initializeStores,
+    Drawer,
+    getDrawerStore,
+  } from "@skeletonlabs/skeleton";
+  import { LightSwitch } from "@skeletonlabs/skeleton";
+  import { onMount } from "svelte";
+  import { invalidate } from "$app/navigation";
+  initializeStores();
 
-	const drawerStore = getDrawerStore();
+  const drawerStore = getDrawerStore();
 
-	function drawerOpen() {
-	drawerStore.open({});
-	}
-
+  function drawerOpen() {
+    drawerStore.open({});
+  }
 </script>
 
 <!-- App Shell -->
 <Drawer><Navigation /></Drawer>
-<AppShell slotSidebarLeft="bg-surface-500/5 w-0 lg:w-64">
-	<svelte:fragment slot="header">
-		<!-- App Bar -->
-		<AppBar>
-			<svelte:fragment slot="lead">
-				<div class="flex items-center">
-					<button class="lg:hidden btn btn-sm mr-4" on:click={drawerOpen}>
-						<span>
-							<svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
-								<rect width="100" height="20" />
-								<rect y="30" width="100" height="20" />
-								<rect y="60" width="100" height="20" />
-							</svg>
-						</span>
-					</button>
-					<strong class="text-xl uppercase">SIVIR.GG</strong>
-				</div>
-			</svelte:fragment>
-			
-			<svelte:fragment slot="trail">
+<AppShell slotsidebarleft="bg-surface-500/5 w-0 lg:w-64">
+  <svelte:fragment slot="header">
+    <!-- App Bar -->
+    <AppBar>
+      <svelte:fragment slot="lead">
+        <div class="flex items-center">
+          <button class="lg:hidden btn btn-sm mr-4" on:click={drawerOpen}>
+            <span>
+              <svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
+                <rect width="100" height="20" />
+                <rect y="30" width="100" height="20" />
+                <rect y="60" width="100" height="20" />
+              </svg>
+            </span>
+          </button>
+          <strong class="text-xl uppercase">SIVIR.GG</strong>
+        </div>
+      </svelte:fragment>
 
-			<LightSwitch />
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SivirGG"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
+      <svelte:fragment slot="trail">
+        <LightSwitch />
+        <a
+          class="btn btn-sm variant-ghost-surface"
+          href="https://twitter.com/SivirGG"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Twitter
+        </a>
+      </svelte:fragment>
+    </AppBar>
+  </svelte:fragment>
 
-			</svelte:fragment>
-		</AppBar>
-	</svelte:fragment>
+  <svelte:fragment slot="sidebarLeft">
+    <Navigation />
+  </svelte:fragment>
 
-	<svelte:fragment slot="sidebarLeft">
-		<Navigation />
-	</svelte:fragment>
-
-
-	<!-- Page Route Content -->
-	<slot />
+  <!-- Page Route Content -->
+  <slot />
 </AppShell>
