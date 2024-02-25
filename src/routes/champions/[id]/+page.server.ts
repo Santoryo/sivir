8031264f4a6d112cb06803fe4b772aa198122358
Santoryo <santoryo.evergarden@gmail.com>
2024-02-25 14:40:32 +0100

    Update +page.server.ts

--- src/routes/champions/[id]/+page.server.ts
+++ src/routes/champions/[id]/+page.server.ts
@@ -6,10 +6,10 @@ export async function load({params}) {
 }
 
 async function init(championName: string) {
-    const response = await fetch('https://cdn.brelshaza.com/json/champions.json', {mode: 'no-cors', method: 'GET', headers: {'Content-Access-Control-Allow-Origin': '*'}})
+    const response = await fetch('https://cdn.brelshaza.com/json/champions.json')
     const champions = await response.json();
 
-    const champ: any = Object.keys(champions).find(o => champions[o].name == championName)
+    const champ: any = Object.keys(champions).find(o => champions[o].name.toLowerCase() == championName.toLowerCase())
 
     return champions[champ];
 
