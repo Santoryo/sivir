4fa10a280d81cb5840f6deebaedc61c4ba6e287e
sant <santoryo.evergarden@gmail.com>
2024-07-26 13:00:11 +0200

    fixes to images

--- src/lib/SkinCard/Skin.svelte
+++ src/lib/SkinCard/Skin.svelte
@@ -8,10 +8,12 @@
     const cdn = "https://raw.communitydragon.org/pbe/plugins/rcp-be-lol-game-data/global/default/assets/loot/";
     const emotecdn = "https://raw.communitydragon.org/latest/game/";
 
+    console.log(skin);
+
 </script>
 
 {#if skin.type == "SKIN"}
-<div class='frame' style='background-image: url("{splash}{skin.championId}/{skin.championId}0{skin.number.toLocaleString('en-US', {minimumIntegerDigits: 2})}.jpg")'>
+<div class='frame' style='background-image: url("//wsrv.nl/?url=raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/assets/characters/{skin.champion.replace("'", "").replace(" ", "").toLowerCase()}/skins/skin{skin.number.toLocaleString('en-US', {minimumIntegerDigits: 2})}/images/{skin.champion.replace("'", "").replace(" ", "").toLowerCase()}_splash_tile_{skin.number}.jpg")'>
     <span class="absolute bottom-0 skincardinfo text-center">{skin.skinName}
         <br> <div class="h-[15px] w-[15px] bg-cover inline-block bg-center" style='background-image: url("/lol/ME.webp")'></div> {skin.price} <span class='font-light'></span></span>
 
@@ -19,7 +21,7 @@
 
 {:else if skin.outputs[0].lootId.includes("EMOTE_")}
 <div class='frame' style='background-image: url("{emotecdn}{skin.imagePath.replace("vfx", "glow")}")'>
-    <div class='frame-under border-none' style='background-image: url("{emotecdn}{skin.imagePath.toLowerCase()}")'></div>
+    <div class='frame-under border-none' style='background-image: url("//wsrv.nl/?url={emotecdn}{skin.imagePath.toLowerCase()}")'></div>
 
     <span class="absolute bottom-0 skincardinfo text-center">{skin.name}
         <br> <div class="h-[15px] w-[15px] bg-cover inline-block bg-center" style='background-image: url("/lol/ME.webp")'></div> {skin.price} <span class='font-light'></span></span>
@@ -34,13 +36,13 @@
 
 </div>
 {:else if skin.name.toLowerCase().includes(" icon")}
-<div class='frame' style='background-image: url("https://ddragon.leagueoflegends.com/cdn/13.24.1/img/profileicon/{skin.outputs[0].lootId.replace('SUMMONER_ICON_', '')}.png'>
+<div class='frame' style='background-image: url("//wsrv.nl/?url=https://ddragon.leagueoflegends.com/cdn/13.24.1/img/profileicon/{skin.outputs[0].lootId.replace('SUMMONER_ICON_', '')}.png'>
     <span class="absolute bottom-0 skincardinfo text-center">{skin.name}
         <br> <div class="h-[15px] w-[15px] bg-cover inline-block bg-center" style='background-image: url("/lol/ME.webp")'></div> {skin.price} <span class='font-light'></span></span>
 
 </div>
 {:else if skin.outputs[0].lootId.includes("WARD_SKIN_")}
-<div class='frame bg-center' style='background-image: url("https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/content/src/leagueclient/wardskinimages/{skin.outputs[0].lootId.replace('WARD_SKIN_', 'wardhero_')}.png")'>
+<div class='frame bg-center' style='background-image: url("//wsrv.nl/?url=https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/content/src/leagueclient/wardskinimages/{skin.outputs[0].lootId.replace('WARD_SKIN_', 'wardhero_')}.png")'>
     <span class="absolute bottom-0 skincardinfo text-center">{skin.name}
         <br> <div class="h-[15px] w-[15px] bg-cover inline-block bg-center" style='background-image: url("/lol/ME.webp")'></div> {skin.price} <span class='font-light'></span></span>
 
--- src/lib/SkinCard/SkinCard.svelte
+++ src/lib/SkinCard/SkinCard.svelte
@@ -9,18 +9,20 @@
 
     const splash = "https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-tiles/";
 
+    console.log(skin);
+
 </script>
 
 
 {#if type == "skin"}
-<div class='frame' style='background-image: url("{splash}{skin.championId}/{skin.championId}0{skin.number.toLocaleString('en-US', {minimumIntegerDigits: 2})}.jpg")'>
+<div class='frame' style='background-image: url("//wsrv.nl/?url=raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/assets/characters/{skin.championName.replace("'", "").replace(" ", "").toLowerCase()}/skins/skin{skin.number.toLocaleString('en-US', {minimumIntegerDigits: 2})}/images/{skin.championName.replace("'", "").replace(" ", "").toLowerCase()}_splash_tile_{skin.number}.jpg")'>
     <span class="absolute bottom-0 skincardinfo text-center">{skin.skinName}
         <br> <div class="h-[15px] w-[15px] bg-cover inline-block bg-center" style='background-image: url("/lol/RP.webp")'></div> {skin.discountedPrice} <span class='font-light'><s>{skin.originalPrice}</s></span></span>
 
 </div>
 
 {:else if type == "champion"}
-<div class='frame' style='background-image: url("{splash}{skin.championId}/{skin.championId}000.jpg")'>
+<div class='frame' style='background-image: url("//wsrv.nl/?url=raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/assets/characters/{skin.name.replace("'", "").replace(" ", "").toLowerCase()}/skins/base/images/{skin.name.replace("'", "").replace(" ", "").toLowerCase()}_splash_tile_0.jpg")'>
     <span class="absolute bottom-0 skincardinfo text-center">{skin.name}
         <br>
         <div class="h-[15px] w-[15px] bg-cover inline-block bg-center shadow-lg" style='background-image: url("/lol/BE.png")'></div> <span class="pr-2">{skin.price.blueEssence}</span>  
@@ -29,13 +31,13 @@
 </div>
 
 {:else if type == "skinline"}
-<div class='frame' style='background-image: url("{splash}{skin.splash}")'>
+<div class='frame' style='background-image: url("//wsrv.nl/?url=raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/{skin.splash.replace("/lol-game-data/assets/", "").toLowerCase()}")'>
     <span class="absolute bottom-0 skincardinfo text-center">{skin.name}</span>
 
 </div>
 
 {:else if type == "skinlineone"}
-<div class='frame' style='background-image: url("{splash}{skin.tilePath.replace("/lol-game-data/assets/v1/champion-tiles/", "").replace("http://", "https://")}")'>
+<div class='frame' style='background-image: url("//wsrv.nl/?url=raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/{skin.tilePath.replace("/lol-game-data/assets/", "").toLowerCase()}")'>
     <span class="absolute bottom-0 skincardinfo text-center">{skin.name}</span>
 
 </div>
