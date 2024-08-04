79f09cfea527ca6b6628cece9c45df88fb5c0e68
sant <santoryo.evergarden@gmail.com>
2024-08-04 16:21:05 +0200

    evil hack

--- src/routes/mythic-shop/[[id]]/+page.svelte
+++ src/routes/mythic-shop/[[id]]/+page.svelte
@@ -18,7 +18,7 @@
 
     <Meta titleSuffix="Mythic Shop Rotation" description={"Mythic Shop Rotation for Patch "} />
     
-    <div class="p-10 flex flex-col items-left lg:items-left">
+    <div class="p-10 flex flex-col items-left lg:items-left" id="container">
         <div class="text-4xl font-bold uppercase w-fit">
             <!-- {#if isCurrent} Current {:else} Past {/if}Mythic Shop -->
              Current Mythic Shop
